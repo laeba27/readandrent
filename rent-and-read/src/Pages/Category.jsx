634be34a715194d4db58a50908c369fffeca6d50
saloni <saloni522@gmail.com>
2023-10-02 09:06:09 +0530

    category section

--- rent-and-read/src/Pages/Category.jsx
+++ rent-and-read/src/Pages/Category.jsx
@@ -1,9 +1,10 @@
 import HeaderCategory from "../Components/categorypage/Headercategory/HeaderCategory"
-
+import BooksCategory from '../Components/categorypage/bookscategory/category'
 const Category = () => {
   return (
     <div>
      <HeaderCategory/> 
+     <BooksCategory />
     </div>
   )
 }
